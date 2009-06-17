0e86f3b2bd18d86f55f554e2982030af9e863c09
John Barnette <jbarnette@gmail.com>
2009-06-17 16:57:39 -0700

    Add initializers for the child plugins.

--- lib/hoe/first.rb
+++ lib/hoe/first.rb
@@ -1,4 +1,8 @@
 module Hoe::First
+  def initialize_first
+    warn "initialize_first"
+  end
+
   def define_first_tasks
     warn "define_first_tasks"
   end
--- lib/hoe/second.rb
+++ lib/hoe/second.rb
@@ -1,4 +1,8 @@
 module Hoe::Second
+  def initialize_second
+    warn "initialize_second"
+  end
+
   def define_second_tasks
     warn "define_second_tasks"
   end
